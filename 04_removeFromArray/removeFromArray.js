2e8acb15404b407795a067ec5423f77c9d10f51d
h-veng <henry.veng@hotmail.com>
2022-09-03 20:14:58 +1000

    Add my solution to 04__removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    if (arguments.length < 2) {
+        return array;
+    }
 
+    let returnArray = [];
+
+    outerLoop: for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < toRemove.length; j++) {
+            if (array[i] === toRemove[j]) {
+                continue outerLoop;
+            }
+        }
+        returnArray.push(array[i]);
+    }
+
+    return returnArray;
 };
 
 // Do not edit below this line
