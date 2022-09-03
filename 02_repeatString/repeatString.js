79d195aff78c9c0a95a1ca273e18759b6dba1321
h-veng <henry.veng@hotmail.com>
2022-09-03 18:47:50 +1000

    Add my solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(baseString, nRepeats) {
+    if (nRepeats < 0) {
+        return "ERROR";
+    }
 
+    let outputString = "";
+    for (let i = 0; i < nRepeats; i++) {
+        outputString += baseString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
