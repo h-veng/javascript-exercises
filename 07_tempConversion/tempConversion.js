4a99cf4c69a2d49c2c46551b43b9113de9f47a76
h-veng <henry.veng@hotmail.com>
2022-09-03 20:58:54 +1000

    Add my solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+// This rounding method seems shoddy according to some responses on stack
+// overflow. However, I'm just going to keep this as the tests were most likely
+// written with this in mind.
+const round1dp = function(number) {
+  return Math.round(number * 10) / 10;
+}
 
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  return round1dp(tempC);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * 9 / 5) + 32;
+  return round1dp(tempF);
 };
 
 // Do not edit below this line
